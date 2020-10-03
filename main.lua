8e7b6e5dd6b9db47a215381c5fb1765c261312a3
Indi <indivicivet@gmail.com>
2020-10-03 02:54:10 +0100

    add task status and colours

--- main.lua
+++ main.lua
@@ -60,10 +60,21 @@ function love.load()
 			str="click the egg!",
 		},
 	}
+	STATUS = {not_done=0, current=1, done=2}
+	TASK_STATUS_COLOURS = {
+		[STATUS.not_done]={1, 1, 1},
+		[STATUS.current]={0.5, 0.5, 1},
+		[STATUS.done]={0.5, 1, 0.8},
+	}
 	
-	tasks = {TASK_TYPES.click_egg, TASK_TYPES.click_egg}
+	tasks = {{tasktype=TASK_TYPES.click_egg}, {tasktype=TASK_TYPES.click_egg}}
+	for i, task in ipairs(tasks) do
+		task.status = STATUS.not_done
+	end
+	tasks[1].status = STATUS.current
 	
 	spawned_eggs = {}
+	t = 0
 	NEXT_EGG_TIME = 1
 	CONVEYOR_SPEED = 80
 	next_egg_timer = NEXT_EGG_TIME
@@ -77,9 +88,14 @@ function love.draw()
 	love.graphics.print("for egg in basket:", text_d_x, text_d_y)
 	text_d_x = text_d_x + 50 -- tab in
 	for i, task in ipairs(tasks) do
+		love.graphics.setColor(TASK_STATUS_COLOURS[task.status])
 		text_d_y = text_d_y + BASE_FONTSIZE * 1.2
-		love.graphics.print(task.str, text_d_x, text_d_y)
+		love.graphics.print(task.tasktype.str, text_d_x, text_d_y)
+		if task.status == STATUS.current and math.floor(t * 2) % 2 == 0 then
+			love.graphics.print(">", text_d_x - 25, text_d_y)
+		end
 	end
+	love.graphics.setColor(1, 1, 1)
 	for i, egg in ipairs(spawned_eggs) do
 		love.graphics.draw(egg.eggtype.sprite, egg.x, egg.y)
 	end
@@ -87,6 +103,7 @@ function love.draw()
 end
 
 function love.update(dt)
+	t = t + dt
 	next_egg_timer = next_egg_timer - dt
 	if next_egg_timer < 0 then
 		if #basket_eggs > 0 then
