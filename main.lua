7f6e8c560bd05a224ef3323b29afc6619a9473e1
Indi <indivicivet@gmail.com>
2020-10-03 02:33:16 +0100

    draw some eggs

--- main.lua
+++ main.lua
@@ -1,5 +1,3 @@
-
-
 function love.load()
 	WIDTH = 1280
 	HEIGHT = 720
@@ -8,6 +6,58 @@ function love.load()
 
 	BASE_FONTSIZE = 22
 	BASE_FONT = love.graphics.newFont("fonts/VCR_OSD_MONO_1.001.ttf", BASE_FONTSIZE)
+	
+	EGG_TYPES = {
+		{
+			col="blue",
+			striped=false,
+			sprite_name="egg_blue_dotty",
+		},
+		{
+			col="green",
+			striped=false,
+			sprite_name="egg_green_dotty",
+		},
+		{
+			col="red",
+			striped=false,
+			sprite_name="egg_red_dotty",
+		},
+		{
+			col="blue",
+			striped=true,
+			sprite_name="egg_blue_striped",
+		},
+		{
+			col="green",
+			striped=true,
+			sprite_name="egg_green_striped",
+		},
+		{
+			col="red",
+			striped=true,
+			sprite_name="egg_red_striped",
+		},
+		{
+			col="gold",
+			striped=false,
+			sprite_name="egg_golden",
+		},
+	}
+	for i, eggtype_info in ipairs(EGG_TYPES) do
+		eggtype_info["sprite"] = love.graphics.newImage(
+			"graphics/" .. eggtype_info["sprite_name"] .. ".png"
+		)
+	end
+	
+	eggs = {}
+	for i = 1, 5 do
+		eggs[#eggs + 1] = {
+			spawned=false,
+			eggtype=EGG_TYPES[i],
+		}
+	end
+	next_egg_timer = 1
 end
 
 function love.draw()
@@ -19,5 +69,15 @@ function love.draw()
 	text_d_y = text_d_y + BASE_FONTSIZE * 1.2
 	text_d_x = text_d_x + 50 -- tab in
 	love.graphics.print("click the egg!", text_d_x, text_d_y)
+	egg_d_y = 500
+	egg_d_x = 50
+	for i, egg in ipairs(eggs) do
+		love.graphics.draw(egg.eggtype.sprite, egg_d_x, egg_d_y)
+		egg_d_x = egg_d_x + 150
+	end
+end
+
+function love.update(dt)
+	
 end
 
